41536688d19d7919ee2c721a6c22ba669dd2c85f
John Ellison <info@johnwebsites.com>
2016-02-29 17:28:59 +0000

    add betterplace lab data source

--- ImportData.js
+++ ImportData.js
@@ -43,17 +43,32 @@ var sources = {
 		type: sourceType.urlSpreadSheet,
 		pathUrl: 'https://docs.google.com/spreadsheets/d/1A5wDMgmddNalWB77dlqWIvl1X-dYBrU-7x3mGBkBkDc/',
 		startRow: 1,
-		identifierIndex: 3, // Name
+		identifierIndex: 4, // Name
 		mappings: [
-			[0, 0], // Country
-			[1, 2], // Project Name
-			[2, 8], // URL
-			[3, 7], // Solution
-			[4, 11], // City
-			[5, 12], // Country
-			[6, 13], // Offices
-			[7, 16], // Category
-			[8, 17] // Tag
+			[0, 12], // Country
+			[1, 11], // City
+			[2, 10], // Address
+			[3, 8], // Website
+			[4, 2], // Project Name
+			[5, 7], // Description
+			[6, 15], // Category
+			[7, 16], // Tags
+			[8, 22] // Facebook
+		]
+	},
+	'digital-refugee-projects-betterplace-lab': {
+		type: sourceType.urlSpreadSheet,
+		pathUrl: 'https://docs.google.com/spreadsheets/d/1t82LzxBH5GL2HOnEySZE6irLXLAI6rogJ-r8Cf573yo/',
+		startRow: 1,
+		identifierIndex: 0, // Name
+		mappings: [
+			[0, 12], // Name
+			[1, 12], // Country
+			[2, 8], // Website
+			[3, 7], // Description
+			[5, 15], // Type
+			[6, 29], // Category (German)
+			[10, 3] // Organization
 		]
 	}
 };
